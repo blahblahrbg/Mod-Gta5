89730fdb1a9fbcb147208c5a7252f3333bb7534e
TrNEX <39301452+TrNEX@users.noreply.github.com>
2018-05-15 22:15:00 +0300

    Add files via upload

---
+++ Autowall.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Interfaces.h"
+
+#pragma once
+
+#include "Hacks.h"
+
+struct FireBulletData
+{
+	FireBulletData(const Vector &eye_pos)
+		: src(eye_pos)
+	{
+	}
+
+	Vector           src;
+	trace_t          enter_trace;
+	Vector           direction;
+	CTraceFilter    filter;
+	float           trace_length;
+	float           trace_length_remaining;
+	float           current_damage;
+	int             penetrate_count;
+};
+bool HandleBulletPenetration(CSWeaponInfo *, FireBulletData &);
+
+bool CanHit(IClientEntity* entity, const Vector &point, float *damage_given);
